ed089629245d819df4db97162c349cb52ad6dea7
drex109 <96746274+drex109@users.noreply.github.com>
2025-05-02 13:30:30 -0600

    added events for drawing on grid and button for changing canvas dimensions

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const grid = document.querySelector('#grid');
+const size = document.querySelector('#size');
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    grid.appendChild(square);
+function createCanvas(canvasSize) {
+    for (let i = 0; i < canvasSize; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', (e) => {
+            square.style.backgroundColor = 'black';
+        });
+        grid.appendChild(square);
+    }
 }
+
+createCanvas(256);
+
+size.addEventListener('click', (e) => {
+    
+    let choice = parseInt(prompt('Please enter a number that does not exceed 100'));
+    if (choice > 0 && choice <= 100) {
+        grid.innerHTML = ''
+        canvas = choice * choice;
+        grid.style.width = `${4.5 * choice}em`;
+
+        createCanvas(canvas);
+
+    } else {
+        alert('Invalid input')
+    }
+});
+
