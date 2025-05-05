1fa2876f6933212c746b4bd66eb4685384b33019
drex109 <96746274+drex109@users.noreply.github.com>
2025-05-05 14:22:45 -0600

    added random color and progressive opacity to mouseover

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 const grid = document.querySelector('#grid');
 const size = document.querySelector('#size');
 
+function randomRgb() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
+}
+
 function createCanvas(canvasChoice) {
     grid.innerHTML = ''
     const totalCanvas = canvasChoice * canvasChoice;
     const squareSize = 100 / canvasChoice;
     for (let i = 0; i < totalCanvas; i++) {
         let square = document.createElement('div');
+        let opa = 10;
         square.style.flexBasis = `${squareSize}%`;
         square.style.aspectRatio = '1 / 1';
         square.style.border = '1px solid black';
         square.addEventListener('mouseover', (e) => {
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = randomRgb();
+            square.style.opacity = `${opa}%`
+            opa += 10;
         });
         grid.appendChild(square);
     }
