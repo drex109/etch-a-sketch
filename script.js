fc4d9985d94443c1fa254f632d4195711ea79656
drex109 <96746274+drex109@users.noreply.github.com>
2025-05-05 14:11:23 -0600

    fixed canvas creation function to resize based on chosen dimensions

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const grid = document.querySelector('#grid');
 const size = document.querySelector('#size');
 
-function createCanvas(canvasSize) {
-    for (let i = 0; i < canvasSize; i++) {
+function createCanvas(canvasChoice) {
+    grid.innerHTML = ''
+    const totalCanvas = canvasChoice * canvasChoice;
+    const squareSize = 100 / canvasChoice;
+    for (let i = 0; i < totalCanvas; i++) {
         let square = document.createElement('div');
+        square.style.flexBasis = `${squareSize}%`;
+        square.style.aspectRatio = '1 / 1';
+        square.style.border = '1px solid black';
         square.addEventListener('mouseover', (e) => {
             square.style.backgroundColor = 'black';
         });
@@ -11,18 +17,13 @@ function createCanvas(canvasSize) {
     }
 }
 
-createCanvas(256);
+createCanvas(16);
 
 size.addEventListener('click', (e) => {
     
     let choice = parseInt(prompt('Please enter a number that does not exceed 100'));
     if (choice > 0 && choice <= 100) {
-        grid.innerHTML = ''
-        canvas = choice * choice;
-        grid.style.width = `${4.5 * choice}em`;
-
-        createCanvas(canvas);
-
+        createCanvas(choice);
     } else {
         alert('Invalid input')
     }
